6fec67480128d29b4e6c00594e72a03dbe11cf7d
Muthukumar.kb <Muthukumar.kb@gmail.com>
2014-04-26 00:25:42 -0700

    test

---
+++ src/main/java/com/mu2/om/om/App.java
@@ -0,0 +1,13 @@
+package com.mu2.om.om;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Om Vignarajaya Nahama!" );
+    }
+}
